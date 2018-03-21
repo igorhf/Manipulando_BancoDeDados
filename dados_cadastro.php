9f96d5d62729b5a854e7414fa527a8545415f5e6
igorhf <igorhf.2@gmail.com>
2018-03-21 08:44:02 -0300

    correção no nome do banco de dados

--- dados_cadastro.php
+++ dados_cadastro.php
@@ -6,7 +6,7 @@
 </head>
 <body>
 	<?php
-    requere_once("bd.inc");
+    include("bd.inc");
 
 	$user=isset($_POST["user"])?$_POST["user"]:"usuario invalido !!!";
 	$senha=isset($_POST["senha"])?$_POST["senha"]:"senha invalida !!!";
@@ -15,20 +15,39 @@
 
 	switch ($opc) {
 	 	case 'INSERIR':
-			$inserir=mysqli_query($conn,"insert into usuario(user,senha,email)value('$user','$senha','$email')");
+			$inserir=mysqli_query($conn,"insert into cadastro(user,senha,email)value('$user','$senha','$email')");
 			mysqli_close($conn);
 			break;
+
 		case 'ALTERAR':
-		    $alterar=mysqli_query($con,"update usuario  set usuario='$user', senha='$senha', email='$email' where id=$id ");
+		    $alterar=mysqli_query($con,"update cadastro  set usuario='$user', senha='$senha', email='$email' where id=$id ");
 		    mysqli_close($conn);
 		break;
+		
 		case 'REMOVER':
-			$deletar=mysqli_query($conn,"delete from usuario where id=$id");
+			$deletar=mysqli_query($conn,"delete from cadastro where id=$id");
 			mysqli_close($conn);
 			break;
+
 		case 'CONSULTAR':
-			# code...
+			$consutar=mysqli_query($conn,"select * from cadastro");
+			while($res=mysqli_fetch_row($consutar)) {
+			$id=$res[0];
+			$usuario=$res[1];
+			$senha=$res[2];
+			$email=$res[3];
+
+		    echo "
+	        ID: $id<br>	   
+	        USUARIO: $usuario<br>
+	        SENHA: $senha<br>
+	        EMAIL: $email<br><hr>
+	        ";
+
+			}
+			mysqli_close($conn);
 			break;
+
 		default:
 			# code...
 			break;
